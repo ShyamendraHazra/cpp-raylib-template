60dd97409f1f5949035149de7da2a06138dcf298
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-08-30 12:47:59 +0530

    reset code to boilerplate

--- src/RAYLIB_TEST.c
+++ src/RAYLIB_TEST.c
@@ -1,57 +1,6 @@
 #include "RAYLIB_TEST.h"
 
 int main(void)
-{   
-    WINDOW win;
-    init_window_data(&win);
-    uint8_t fonstSize = 25;
-    TEXT heading;
-    Rectangle Rect_One;
-
-
-    InitWindow(win.X,win.Y, "RAYLIB MK I");
-    
-    init_text_object(&heading,\
-        "Congrats! You created your first window!",\
-        fonstSize,\
-        win
-    );
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        DrawRectangleRounded(\
-            (Rectangle){\
-            (win.X - (win.X/2 + 10)),\
-            (win.Y - (win.Y/2 + 10)),\
-            200,\
-            200},\
-            0.08,100,RED\
-        );
-        
-        DrawText(heading.val, heading.current_X1, heading.current_Y1, fonstSize, WHITE);
-
-        heading.current_X1 += heading.X_change;
-        heading.current_Y1 += heading.Y_change;
-
-        heading.current_X2 += heading.X_change;
-        heading.current_Y2 += heading.Y_change;
-
-        if (heading.current_X1 <= 0 || heading.current_X2 >=  win.X) {
-            heading.X_change = - heading.X_change;
-        }
-        
-        if (heading.current_Y1 <= 0 || heading.current_Y2 >=  win.Y) {
-            heading.Y_change = - heading.Y_change;
-        }
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
+{  
     return 0;
 }
--- src/RAYLIB_TEST.h
+++ src/RAYLIB_TEST.h
@@ -6,45 +6,4 @@
     #include <stdio.h>
     #include <string.h>
 
-
-    typedef struct {
-        uint16_t X;
-        uint16_t Y;
-        uint16_t CX;
-        uint16_t CY;
-    } WINDOW;
-
-    typedef struct {
-        char* val;
-        uint8_t length;
-        uint16_t current_X1;
-        uint16_t current_X2;
-        uint16_t current_Y1;
-        uint16_t current_Y2;
-        int8_t X_change;
-        int8_t Y_change;
-        uint8_t font_size;
-        uint16_t text_bound_x;
-        uint16_t text_bound_y;
-    } TEXT;
-
-
-    typedef struct{
-        Rectangle* rectp;
-        uint16_t border_radius;
-        int8_t X_change;
-        int8_t Y_change;
-    }RectangleData;
-
-    typedef struct{
-        uint16_t X;
-        uint16_t Y;
-    } CoOrdinate;
-
-
-    void init_window_data(WINDOW* win);
-    void init_text_object(TEXT* text, char* string, uint8_t font_size, WINDOW win);
-    void print_text_object(TEXT* text);
-    void init_rectangle(RectangleData* rect, CoOrdinate coor, WINDOW win);
-    void print_font_meta(Font* font);
 #endif
