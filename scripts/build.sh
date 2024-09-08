22d750c887858dedf143750b82227e6c5123b36f
Shyamendra Hazra <shyamendrahazra@gmail.com>
2024-09-08 16:45:12 +0530

    Updated: to adapt to new CMakeLists.txt file location in cmake dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@ echo -e "Building binaries :: \n"
 
 echo -e "Building binary for Linux :: \n"
 
-cmake -S . -B build/Linux
+cmake -S cmake -B build/Linux
 
 echo -e "Building binary for Windows :: \n"
 
-cmake -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake -S . -B build/Windows
+cmake -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake -S cmake -B build/Windows
